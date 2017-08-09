ec7123ab748b62f7e48f25c2b6ebab5dc2f3ddb3
TheEnAa <kostek4321@o2.pl>
2017-08-09 13:10:32 +0200

    Calculator

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,32 @@
 package com.kodilla.testing;
 
-class TestingMain {
+import com.kodilla.testing.calculator.Calculator;
+import com.kodilla.testing.user.SimpleUser;
+
+public class TestingMain {
     public static void main(String[] args){
-        System.out.println("Moduł 6. Wprowadzenie to testowania oprogramowania");
+        SimpleUser simpleUser = new SimpleUser("theForumUser");
+
+        String result = simpleUser.getUsername();
+
+        if (result.equals("theForumUser")){
+            System.out.println("test OK");
+        } else {
+            System.out.println("Error!");
+        }
+        Calculator calculator = new Calculator(5,10);
+
+        if(calculator.getAdd() == 5 + 10){
+            System.out.println("Ok");
+        }
+        else {
+            System.out.println("Error");
+        }
+        if(calculator.getSubtract() == 5 - 10){
+            System.out.println("Ok");
+        }
+        else {
+            System.out.println("Error");
+        }
     }
 }
