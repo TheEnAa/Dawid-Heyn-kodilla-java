4853efc29ee6caba5c1dd3e67fac084e6c003faf
TheEnAa <kostek4321@o2.pl>
2017-08-09 12:07:36 +0200

    Testing

--- src/main/java/com/kodilla/checkers/GameBoard.java
+++ src/main/java/com/kodilla/checkers/GameBoard.java
@@ -1,18 +1,9 @@
 package com.kodilla.checkers;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class GameBoard {
-    ArrayList<String> list;
 
-    public GameBoard() {
-        list = new ArrayList<String>(10);
-    }
-    public void addList(){
-        for(int i=0;i<10;i++)
-            list.add(new ArrayList<String>(10));
-    }
-public static void main (String[] args){
-        GameBoard board = new GameBoard();
-        board.addList();
-    }
+}
+
